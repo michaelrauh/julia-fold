b73dd02b1150605d1a0623cb0ae9c8fc770adce7
michaelrauh <michaelrauh@ymail.com>
2021-07-04 18:45:05 -0400

    make get all prevs function

--- atom.jl
+++ atom.jl
@@ -37,3 +37,17 @@ function read_file_to_arrays(filename :: String) :: Vector{Vector{String}}
 end
 
 read_file_to_arrays("example1.txt")
+
+function get_all_prevs(l :: Vector{Vector{String}}) :: Dict{String, Set{String}}
+    mergewith(union, map(get_prevs, l)...)
+end
+
+function get_prevs(l :: Vector{String}) :: Dict{String, Set{String}}
+    if length(l) < 2
+        Dict()
+    else
+        mergewith(union, Dict(l[2]=>Set([first(l)])), get_prevs(l[2:end]))
+    end
+end
+
+get_all_prevs([["a", "b", "c"], ["d", "e", "f"], ["e", "b"]])
